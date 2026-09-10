f7b1365b3713caab15e19a97c73ddeb3c3974e8a
ultavladi1 <vladislavchev16@gmail.com>
2026-09-10 23:07:16 +0300

    Fix VoidBrute renderer Biped render state type

--- src/client/java/net/voidstalker/client/render/VoidBruteRenderer.java
+++ src/client/java/net/voidstalker/client/render/VoidBruteRenderer.java
@@ -4,11 +4,11 @@ import net.minecraft.client.render.entity.EntityRendererFactory;
 import net.minecraft.client.render.entity.MobEntityRenderer;
 import net.minecraft.client.render.entity.model.BipedEntityModel;
 import net.minecraft.client.render.entity.model.EntityModelLayers;
-import net.minecraft.client.render.entity.state.LivingEntityRenderState;
+import net.minecraft.client.render.entity.state.BipedEntityRenderState;
 import net.minecraft.util.Identifier;
 import net.voidstalker.entity.custom.VoidBruteEntity;
 
-public class VoidBruteRenderer extends MobEntityRenderer<VoidBruteEntity, LivingEntityRenderState, BipedEntityModel<LivingEntityRenderState>> {
+public class VoidBruteRenderer extends MobEntityRenderer<VoidBruteEntity, BipedEntityRenderState, BipedEntityModel<BipedEntityRenderState>> {
     private static final Identifier TEXTURE = Identifier.of("voidstalker", "textures/entity/void_brute.png");
 
     public VoidBruteRenderer(EntityRendererFactory.Context context) {
@@ -16,7 +16,12 @@ public class VoidBruteRenderer extends MobEntityRenderer<VoidBruteEntity, Living
     }
 
     @Override
-    public Identifier getTexture(LivingEntityRenderState state) {
+    public BipedEntityRenderState createRenderState() {
+        return new BipedEntityRenderState();
+    }
+
+    @Override
+    public Identifier getTexture(BipedEntityRenderState state) {
         return TEXTURE;
     }
 }
