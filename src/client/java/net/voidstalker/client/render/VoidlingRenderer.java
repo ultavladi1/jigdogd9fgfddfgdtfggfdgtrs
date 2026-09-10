607b25d950af34e854c7837ed3db30cc17ee661e
ultavladi1 <vladislavchev16@gmail.com>
2026-09-10 23:07:06 +0300

    Fix Voidling renderer Biped render state type

--- src/client/java/net/voidstalker/client/render/VoidlingRenderer.java
+++ src/client/java/net/voidstalker/client/render/VoidlingRenderer.java
@@ -4,11 +4,11 @@ import net.minecraft.client.render.entity.EntityRendererFactory;
 import net.minecraft.client.render.entity.MobEntityRenderer;
 import net.minecraft.client.render.entity.model.BipedEntityModel;
 import net.minecraft.client.render.entity.model.EntityModelLayers;
-import net.minecraft.client.render.entity.state.LivingEntityRenderState;
+import net.minecraft.client.render.entity.state.BipedEntityRenderState;
 import net.minecraft.util.Identifier;
 import net.voidstalker.entity.custom.VoidlingEntity;
 
-public class VoidlingRenderer extends MobEntityRenderer<VoidlingEntity, LivingEntityRenderState, BipedEntityModel<LivingEntityRenderState>> {
+public class VoidlingRenderer extends MobEntityRenderer<VoidlingEntity, BipedEntityRenderState, BipedEntityModel<BipedEntityRenderState>> {
     private static final Identifier TEXTURE = Identifier.of("voidstalker", "textures/entity/voidling.png");
 
     public VoidlingRenderer(EntityRendererFactory.Context context) {
@@ -16,7 +16,12 @@ public class VoidlingRenderer extends MobEntityRenderer<VoidlingEntity, LivingEn
     }
 
     @Override
-    public Identifier getTexture(LivingEntityRenderState state) {
+    public BipedEntityRenderState createRenderState() {
+        return new BipedEntityRenderState();
+    }
+
+    @Override
+    public Identifier getTexture(BipedEntityRenderState state) {
         return TEXTURE;
     }
 }
