b349ab214c5c149bc7dcc671cdbf69cd9048e679
ultavladi1 <vladislavchev16@gmail.com>
2026-09-10 23:01:33 +0300

    Fix Ancient Relic cooldown API for Minecraft 1.21.4

--- src/main/java/net/voidstalker/item/AncientRelicItem.java
+++ src/main/java/net/voidstalker/item/AncientRelicItem.java
@@ -21,14 +21,14 @@ public class AncientRelicItem extends Item {
     @Override
     public ActionResult use(World world, PlayerEntity player, Hand hand) {
         ItemStack stack = player.getStackInHand(hand);
-        if (player.getItemCooldownManager().isCoolingDown(this)) {
+        if (player.getItemCooldownManager().isCoolingDown(stack)) {
             return ActionResult.FAIL;
         }
         if (!world.isClient) {
             player.addStatusEffect(new StatusEffectInstance(StatusEffects.NIGHT_VISION, 20 * 30, 0));
             player.addStatusEffect(new StatusEffectInstance(StatusEffects.RESISTANCE, 20 * 10, 1));
             world.playSound(null, player.getBlockPos(), SoundEvents.BLOCK_BEACON_ACTIVATE, SoundCategory.PLAYERS, 0.6f, 1.4f);
-            player.getItemCooldownManager().set(this, COOLDOWN_TICKS);
+            player.getItemCooldownManager().set(stack, COOLDOWN_TICKS);
         }
         return ActionResult.SUCCESS;
     }
