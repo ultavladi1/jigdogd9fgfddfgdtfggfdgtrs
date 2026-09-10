e9b82bf54a30087952488cdbe944a831a5f6b2d8
ultavladi1 <vladislavchev16@gmail.com>
2026-09-10 23:04:33 +0300

    Fix VoidWatcher renderer for Minecraft 1.21.4 render-state API

--- src/client/java/net/voidstalker/client/render/VoidWatcherRenderer.java
+++ src/client/java/net/voidstalker/client/render/VoidWatcherRenderer.java
@@ -1,22 +1,23 @@
 package net.voidstalker.client.render;
 
-import net.minecraft.client.render.entity.MobEntityRenderer;
 import net.minecraft.client.render.entity.EntityRendererFactory;
+import net.minecraft.client.render.entity.MobEntityRenderer;
+import net.minecraft.client.render.entity.model.BipedEntityModel;
 import net.minecraft.client.render.entity.model.EntityModelLayers;
-import net.minecraft.client.render.entity.model.ZombieEntityModel;
+import net.minecraft.client.render.entity.state.LivingEntityRenderState;
 import net.minecraft.util.Identifier;
 import net.voidstalker.entity.custom.VoidWatcherEntity;
 
 /** Placeholder floating-humanoid look until a bespoke model is made. */
-public class VoidWatcherRenderer extends MobEntityRenderer<VoidWatcherEntity, ZombieEntityModel<VoidWatcherEntity>> {
+public class VoidWatcherRenderer extends MobEntityRenderer<VoidWatcherEntity, LivingEntityRenderState, BipedEntityModel<LivingEntityRenderState>> {
     private static final Identifier TEXTURE = Identifier.of("voidstalker", "textures/entity/void_watcher.png");
 
     public VoidWatcherRenderer(EntityRendererFactory.Context context) {
-        super(context, new ZombieEntityModel<>(context.getPart(EntityModelLayers.ZOMBIE)), 0.4f);
+        super(context, new BipedEntityModel<>(context.getPart(EntityModelLayers.ZOMBIE)), 0.4f);
     }
 
     @Override
-    public Identifier getTexture(VoidWatcherEntity entity) {
+    public Identifier getTexture(LivingEntityRenderState state) {
         return TEXTURE;
     }
 }
