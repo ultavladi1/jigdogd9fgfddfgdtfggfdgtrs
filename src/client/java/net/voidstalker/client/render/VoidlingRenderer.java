7a7558446d9f4f7aba693db05e6066a072b52f12
ultavladi1 <vladislavchev16@gmail.com>
2026-09-10 23:04:27 +0300

    Fix Voidling renderer for Minecraft 1.21.4 render-state API

--- src/client/java/net/voidstalker/client/render/VoidlingRenderer.java
+++ src/client/java/net/voidstalker/client/render/VoidlingRenderer.java
@@ -1,21 +1,22 @@
 package net.voidstalker.client.render;
 
-import net.minecraft.client.render.entity.MobEntityRenderer;
 import net.minecraft.client.render.entity.EntityRendererFactory;
+import net.minecraft.client.render.entity.MobEntityRenderer;
+import net.minecraft.client.render.entity.model.BipedEntityModel;
 import net.minecraft.client.render.entity.model.EntityModelLayers;
-import net.minecraft.client.render.entity.model.ZombieEntityModel;
+import net.minecraft.client.render.entity.state.LivingEntityRenderState;
 import net.minecraft.util.Identifier;
 import net.voidstalker.entity.custom.VoidlingEntity;
 
-public class VoidlingRenderer extends MobEntityRenderer<VoidlingEntity, ZombieEntityModel<VoidlingEntity>> {
+public class VoidlingRenderer extends MobEntityRenderer<VoidlingEntity, LivingEntityRenderState, BipedEntityModel<LivingEntityRenderState>> {
     private static final Identifier TEXTURE = Identifier.of("voidstalker", "textures/entity/voidling.png");
 
     public VoidlingRenderer(EntityRendererFactory.Context context) {
-        super(context, new ZombieEntityModel<>(context.getPart(EntityModelLayers.ZOMBIE)), 0.25f);
+        super(context, new BipedEntityModel<>(context.getPart(EntityModelLayers.ZOMBIE)), 0.25f);
     }
 
     @Override
-    public Identifier getTexture(VoidlingEntity entity) {
+    public Identifier getTexture(LivingEntityRenderState state) {
         return TEXTURE;
     }
 }
