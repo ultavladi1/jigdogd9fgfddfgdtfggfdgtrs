8e37a6dbabf334e630a010e220a163ced906a978
ultavladi1 <vladislavchev16@gmail.com>
2026-09-10 23:04:40 +0300

    Fix VoidBrute renderer for Minecraft 1.21.4 render-state API

--- src/client/java/net/voidstalker/client/render/VoidBruteRenderer.java
+++ src/client/java/net/voidstalker/client/render/VoidBruteRenderer.java
@@ -1,21 +1,22 @@
 package net.voidstalker.client.render;
 
-import net.minecraft.client.render.entity.MobEntityRenderer;
 import net.minecraft.client.render.entity.EntityRendererFactory;
+import net.minecraft.client.render.entity.MobEntityRenderer;
+import net.minecraft.client.render.entity.model.BipedEntityModel;
 import net.minecraft.client.render.entity.model.EntityModelLayers;
-import net.minecraft.client.render.entity.model.ZombieEntityModel;
+import net.minecraft.client.render.entity.state.LivingEntityRenderState;
 import net.minecraft.util.Identifier;
 import net.voidstalker.entity.custom.VoidBruteEntity;
 
-public class VoidBruteRenderer extends MobEntityRenderer<VoidBruteEntity, ZombieEntityModel<VoidBruteEntity>> {
+public class VoidBruteRenderer extends MobEntityRenderer<VoidBruteEntity, LivingEntityRenderState, BipedEntityModel<LivingEntityRenderState>> {
     private static final Identifier TEXTURE = Identifier.of("voidstalker", "textures/entity/void_brute.png");
 
     public VoidBruteRenderer(EntityRendererFactory.Context context) {
-        super(context, new ZombieEntityModel<>(context.getPart(EntityModelLayers.ZOMBIE)), 0.6f);
+        super(context, new BipedEntityModel<>(context.getPart(EntityModelLayers.ZOMBIE)), 0.6f);
     }
 
     @Override
-    public Identifier getTexture(VoidBruteEntity entity) {
+    public Identifier getTexture(LivingEntityRenderState state) {
         return TEXTURE;
     }
 }
